d77c6f89b703a97251856a9010cb1b797c46d01f
Roman Pavlenko <rpavlenko.mail@gmail.com>
2020-10-26 22:43:12 +0100

    feat: initial commit, add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg';
+import signImg from '../image/sign.svg';
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299B01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+font-size: 24px;
+margin-left: 15px`;
+
+const ImgLogo = styled.img`
+  width: 50px
+`;
+
+const ImgSign = styled.img`
+  margin-bottom: 5px;
+  width: 32px;
+  height: 32px;
+`;
+
+const Login = styled.button`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #fff;
+  text-transform: uppercase;
+  background: transparent;
+
+  :hover {
+    transition: 0.2s;
+    color: darkgreen;
+  }
+`;
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={logoImg} alt="logo"/>
+      <H1>MrDonald's</H1>
+    </Logo>
+    <Login>
+      <ImgSign src={signImg} />
+      Войти
+    </Login>
+  </NavBarStyled>
+);
