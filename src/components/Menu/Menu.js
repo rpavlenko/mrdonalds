1ec216ae0a956aadc6415835a3a8f691b87581a0
Roman Pavlenko <rpavlenko.mail@gmail.com>
2020-11-11 00:21:01 +0100

    feat: add useFetch and DB.json + spinner component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
-import dbMenu from '../DBMenu';
+import { useFetch } from '../Hooks/useFetch';
+import Spinner from '../Styled/Spinner';
 
 const MenuStyled = styled.main`
   margin-top: 80px;
@@ -14,23 +15,35 @@ const Section = styled.section`
   padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <Banner />
-    <Section>
-      <h2>Бургеры</h2>
-      <ListItem 
-        itemList={dbMenu.burger}
-        setOpenItem={setOpenItem}  
-      />
-    </Section>
+export const Menu = ({ setOpenItem }) => {
+  const res = useFetch();
 
-    <Section>
-      <h2>Закуски / Напитки</h2>
-      <ListItem 
-        itemList={dbMenu.other}
-        setOpenItem={setOpenItem}    
-      />
-    </Section>
-  </MenuStyled>
-);
+  const dbMenu = res.response;
+
+  return (
+    <MenuStyled>
+      <Banner />
+      { res.response ? 
+      <>
+        <Section>
+          <h2>Бургеры</h2>
+          <ListItem
+            itemList={dbMenu.burger}
+            setOpenItem={setOpenItem}
+          />
+        </Section>
+
+        <Section>
+          <h2>Закуски / Напитки</h2>
+          <ListItem
+            itemList={dbMenu.other}
+            setOpenItem={setOpenItem}
+          />
+        </Section>
+      </> : res.error ? 
+      <div>Sorry, we will fix it</div> : 
+        <Spinner />
+      }
+    </MenuStyled>
+  )
+};
